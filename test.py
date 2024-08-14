07cad7966d284341a311a9e7bee264a7aae1c3a3
memsharded <james@conan.io>
2024-08-14 18:27:07 +0200

    wip

--- test.py
+++ test.py
@@ -3,27 +3,34 @@ import platform
 
 
 def run(cmd):
+    print("*"*50)
+    print("**** RUNNING: ", cmd)
+    print("*"*50)
     ret = os.system(cmd)
     if ret != 0:
         raise Exception(f"Command failed {cmd}")
 
 if platform.system() == "Windows":
     # CMake
-    run("conan install --requires=openssl/[*] -of=myapp/cmake/build -g CMakeToolchain -g CMakeDeps")
-    run("cmake -S myapp/cmake -B myapp/cmake/build -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake")
-    run("cmake --build myapp/cmake/build --config Release")
-    run(r".\myapp\cmake\build\Release\myapp.exe")
+    os.chdir("myapp/cmake")
+    run("conan install .")
+    run("cmake --preset conan-default")
+    run("cmake --build --preset conan-release")
+    run(r".\build\Release\myapp.exe")
 
     # Meson
-    run("conan install --requires=openssl/[*] --tool-requires=pkgconf/[*] -of=myapp/meson/build -g MesonToolchain -g PkgConfigDeps")
-    run(r".\myapp\meson\build\conanbuild.bat && meson setup --native-file=myapp/meson/build/conan_meson_native.ini myapp/meson/build myapp/meson")
-    run(r".\myapp\meson\build\conanbuild.bat && meson compile -C myapp/meson/build")
-    run(r".\myapp\meson\build\myapp.exe")
+    os.chdir("../meson")
+    run("conan install . -of=build")
+    run(r".\build\conanbuild.bat && meson setup --native-file=build/conan_meson_native.ini build .")
+    run(r".\build\conanbuild.bat && meson compile -C build")
+    run(r".\build\myapp.exe")
 
     # Visual Studio
-    run("conan install --requires=openssl/[*] -of=myapp/visual/build -g MSBuildDeps -g MSBuildToolchain")
-    run(r".\myapp\visual\build\conanbuild.bat && msbuild myapp/visual/myapp.sln /p:Configuration=Release /p:Platform=x64")
-    run(r".\myapp\visual\x64\Release\myapp.exe")
+    os.chdir("../visual")
+    run("conan install .")
+    # Just open VS IDE
+    run(r".\conan\conanbuild.bat && msbuild myapp.sln /p:Configuration=Release /p:Platform=x64")
+    run(r".\x64\Release\myapp.exe")
 else:
     # Makefiles (only Linux)
     run("conan install --requires=openssl/[*] -of=myapp/make/build -g GnuToolchain -g MakeDeps")
