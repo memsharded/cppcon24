e8c247ce214135bd532436766eb8df44a9caa3a9
memsharded <james@conan.io>
2024-08-17 22:04:44 +0200

    wip

--- myapp/myapp.cpp
+++ myapp/myapp.cpp
@@ -9,6 +9,7 @@ int main(){
     char string[] = "happy";
 
     SHA256((unsigned char*)&string, strlen(string), (unsigned char*)&sha256_digest);
+    // print sha256_digest into sha256_string as readable str
     for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         snprintf(&sha256_string[i*2], sizeof(sha256_string)-i*2, "%02x", (unsigned int)sha256_digest[i]);
     }
